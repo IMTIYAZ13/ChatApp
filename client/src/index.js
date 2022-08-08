b599055937a09dbd434cc09d261bbddad2b6ebbb
IMTIYAZ13 <imtiyazali828708@gmail.com>
2022-08-09 00:37:17 +0530

    register form added

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './component/App/App';
 
 export const DOMAIN = "http://localhost:3000";
+export const NO_IMAGES = 9; //There are 9 static images from which teh user can choose.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
