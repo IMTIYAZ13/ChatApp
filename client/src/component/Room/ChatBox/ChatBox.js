ee9f82e8e78caf393d4275549296f9118dab7b3b
IMTIYAZ13 <imtiyazali828708@gmail.com>
2022-08-26 10:23:44 +0530

    chatbox added

---
+++ client/src/component/Room/ChatBox/ChatBox.js
@@ -0,0 +1,68 @@
+import Paper from '@mui/material/Paper';
+import SendIcon from '@mui/icons-material/Send';
+import styles from '../Room.module.css';
+import {useEffect, useState} from "react";
+import { meet_code } from "../room";
+import io from "socket.io-client";
+
+const socket = io('/');
+
+const ChatBox = (props) => {
+
+    const [inputValue, setInputValue] = useState('');
+    const [messageReceived, setMessageReceived] = useState([]);
+
+    useEffect(() => {
+        socket.emit("join_room", meet_code);
+        socket.emit("send_message", { message: `Welcome to meet ${meet_code}`, meet_code });
+    }, [])
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    useEffect(() => {
+        socket.on("receive_message", (data) => {
+            setMessageReceived((prev) => {
+                return [...prev, data.message]
+            });
+        });
+    }, [socket]);
+
+    useEffect(() => {
+        if (inputValue !== '') {
+            document.getElementsByName('sendIcon')[0].classList.add(styles.colorAqua);
+        } else {
+            document.getElementsByName('sendIcon')[0].classList.remove(styles.colorAqua);
+        }
+    }, [inputValue]);
+
+    const sendMessage = () => {
+        if (inputValue !== '') {
+            socket.emit("send_message", { message: inputValue, meet_code });
+        }
+    }
+
+    const paraItems = messageReceived.map((message) =>
+        <li>{message}</li>
+    );
+
+    return (
+        <Paper
+            elevation={4}
+            className={styles.chatBox}
+            name={props.name}
+        >
+            Hello World.
+            <p style={{maxHeight: "75%", overflow: "scroll"}}>
+                {paraItems}
+            </p>
+            <div className={styles.inputGroup}>
+                <input className={styles.chatInput} name="inputMessage" value={inputValue} onChange={handleInputChange}/>
+                <SendIcon className={styles.chatInputSendIcon} name={"sendIcon"} onClick={sendMessage}/>
+            </div>
+        </Paper>
+    )
+}
+
+export default ChatBox;
--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const cors = require("cors");
 const socket_io = require("socket.io");
 const http = require("http");
 
-
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({extended: true}));
@@ -15,7 +14,6 @@ app.use(cors({ origin: true }));
 
 const server = http.createServer(app);
 const io = socket_io(server);
-
 
 mongoose
     .connect('mongodb+srv://admin:'+process.env.MONGODB_PASS+'@cluster0.nh0xtdu.mongodb.net/?retryWrites=true&w=majority',{
@@ -38,11 +36,11 @@ io.on('connection', (socket) => {
     console.log(`${socket.id} has joined...`);
     // If user enters meet
     socket.on("join_room", (messagePayload) => {
-        console.log(messagePayload);
         socket.join(messagePayload);
     });
     socket.on("send_message", (data) => {
-        socket.to(data.code).emit("receive_message", data);
+        console.log(data);
+        socket.to(data.meet_code).emit("receive_message", data);
     });
 })
 
